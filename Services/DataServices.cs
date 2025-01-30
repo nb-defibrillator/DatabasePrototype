1bbc7c1d55fc6aa117ab819ff27bff1daf327d94
nb-defibrillator <bokurimono@gmail.com>
2025-01-30 13:43:46 -0600

    Add project files.

---
+++ Services/DataServices.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Services
+{
+    public class DatabaseService
+    {
+        private SQLiteAsyncConnection _database;
+
+        public async Task Initialize()
+        {
+            if (_database != null) return;
+
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "localdb.db");
+            _database = new SQLiteAsyncConnection(dbPath);
+
+
+            await _database.CreateTableAsync<DatabasePrototype.Models.DataItem>();
+        }
+
+        public async Task<List<DatabasePrototype.Models.DataItem>> GetItemsAsync()
+        {
+            await Initialize();
+
+            return await _database.Table<DatabasePrototype.Models.DataItem>().ToListAsync();
+        }
+
+        public async Task<int> SaveItemAsync(DatabasePrototype.Models.DataItem item)
+        {
+            await Initialize();
+            item.LastUpdated = DateTime.Now;
+            return await _database.InsertAsync(item);
+        }
+    }
+}
